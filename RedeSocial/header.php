b8795500477f38cca9853df6d55dc84836ff9217
Rui Moreira <rui_moreira25@outlook.com>
2014-05-02 22:55:26 +0100

    Header Config

---
+++ RedeSocial/header.php
@@ -0,0 +1,10 @@
+
+<div class="content-top-navigator">
+  <ul class="ul_top-menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Inserir</a></li>
+    <li class='active'><a href="#">Gerir</a></li>
+    <li><a href="#">Listar</a></li>
+    <li><a href="#">Apagar</a></li>
+  </ul>
+</div>
