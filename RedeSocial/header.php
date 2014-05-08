d330db6e1e089dfab4170d73846e57ab4615621a
Rui Moreira <rui_moreira25@outlook.com>
2014-05-08 19:33:36 +0100

    add icons

--- RedeSocial/header.php
+++ RedeSocial/header.php
@@ -1,10 +1,34 @@
 
-<div class="content-top-navigator">
-  <ul class="ul_top-menu">
+<div class="content-top-navigator" style="padding-top:8px;color:#00A0EB">
+  <table cellspacing="0" border="0" width="1024">
+  	<tr>
+    	<td width="60">
+        	<img src="img/home.png" width="30" height="30" />
+        	Home
+  		</td>
+        <td width="60">
+  			<img src="img/group.png" width="30" height="30" />
+  			Friends
+  		</td>
+        <td width="60" >
+        <img src="img/menu.png" width="30" height="30" style="margin-left:15px;" />
+        	Notifications
+        </td>
+        <td width="900" border="1">
+        </td>
+        <td>
+  			<img src="img/settings.png" width="30" height="30" />
+  		</td>
+    </tr>
+  </table>
+  
+  <!--<ul class="ul_top-menu">
     <li><a href="#">Home</a></li>
     <li><a href="#">Inserir</a></li>
     <li class='active'><a href="#">Gerir</a></li>
     <li><a href="#">Listar</a></li>
     <li><a href="#">Apagar</a></li>
-  </ul>
+  </ul>-->
+  
+  
 </div>
