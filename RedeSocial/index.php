9f8f64552cc95948b6640dd5d155c74235e6f576
Rui Moreira <rui_moreira25@outlook.com>
2014-05-10 19:13:26 +0100

    ficheiro index.php

---
+++ RedeSocial/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<script type="text/javascript" src="js/script.js"></script>
+<title>gatos.com</title>
+</head>
+
+<body style="margin:0 auto;">
+<div class="Top-Navigator" style="height:75px"><?php include "header.php"; ?></div>
+<div class="Content">
+	<div class="Content_total"><?php include "content.php"; ?></div>
+</div>
+</body>
+</html>
