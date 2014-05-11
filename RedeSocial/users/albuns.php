8a7558460dea385ec5f7215e8dd398abb0194c83
João Pinto <epilif81_@hotmail.com>
2014-05-11 03:21:48 +0100

    albuns e edicao do perfil

--- RedeSocial/users/albuns.php
+++ RedeSocial/users/albuns.php
@@ -1,1 +1,24 @@
-Albuns
+
+<table>
+<tr><td><a href="?pg=home&pg_m=albuns&funcao=albuns"><img src="../img/img_foto.png" /></a></td><td><a href="?pg=home&pg_m=albuns&funcao=criar_albuns"><img src="../img/criar_albuns.png" /></a></td></tr>
+</table>
+
+<?php if($funcao=='albuns'){ ?>
+<div class="title">Àlbuns</div>
+<?php } ?>
+	
+    
+<?php if($funcao=='criar_albuns'){ ?>
+<div class="title">Criar Àlbuns</div>
+    <form action="?pg=home&pg_m=albuns&funcao=inser_albuns" method="post">
+    	<tr><td>Nome:</td><td><input type="text" name="nome" /></td></tr>
+        <tr><td><input type="submit" value="Criar Àlbuns"</td></tr>
+    </form>
+<?php } ?>
+<?php if($funcao=='inser_albuns'){
+	
+	$nome=$_POST['nome'];
+	
+	$sql=mysql_query("insert into eventos values('', '$data', '$local', '$image1', '$inscricao', '$total', '$image', '')");
+					echo "<meta http-equiv=refresh content='0; url=index.php?pg=eventos'>";	
+}?>
--- RedeSocial/users/informacoes.php
+++ RedeSocial/users/informacoes.php
@@ -5,14 +5,13 @@ else
 $funcao='';
 ?>
 <?php if($funcao!='alterar'){ ?>
-Sobre
 
-<br />
 
-Informação Básica
+<div class="title">Informação Básica</div>
 
 <?php $sql=mysql_query("select * from users where id_user='1'");
 while($linha=mysql_fetch_array($sql)){
+	$id_user=$linha['id_user'];
 	$nome=$linha['nome'];
 	$data_nascimento=$linha['data_nascimento'];
 	$email=$linha['email'];
@@ -23,16 +22,64 @@ while($linha=mysql_fetch_array($sql)){
 
 }
 ?>
-<table cellpadding="5" cellspacing="5">
-<tr><td>Nome:</td><td><?php echo $nome; ?></td></tr>
-<tr><td>Email:</td><td><?php echo $email; ?></td></tr>
-<tr><td>Morada:</td><td><?php echo $morada; ?></td></tr>
-<tr><td>Contacto:</td><td><?php echo $contacto; ?></td></tr>
-<tr><td>Sexo:</td><td><?php echo $sexo; ?></td></tr>
-<tr><td>Data de Nascimento:</td><td><?php echo $data_nascimento; ?></td></tr>
-<tr><td>Estado Civil:</td><td><?php echo $estado_civil; ?></td></tr>
-
+<table cellpadding="5" cellspacing="5" border="0">
+<tr><td><strong>Nome:</strong></td><td><?php echo $nome; ?></td></tr>
+<tr><td><strong>Email:</strong></td><td><?php echo $email; ?></td></tr>
+<tr><td><strong>Morada:</strong></td><td><?php echo $morada; ?></td></tr>
+<tr><td><strong>Contacto:</strong></td><td><?php echo $contacto; ?></td></tr>
+<tr><td><strong>Sexo:</strong></td><td><?php echo $sexo; ?></td></tr>
+<tr><td><strong>Data de Nascimento:</strong></td><td><?php echo $data_nascimento; ?></td></tr>
+<tr><td><strong>Estado Civil:</strong></td><td><?php echo $estado_civil; ?></td></tr>
+<tr><td><a href="?pg=home&pg_m=informacoes&funcao=alterar&id=<?php echo $id_user; ?>">Alterar</a></td></tr>
 </table>
 <?php } ?>
+<?php if($funcao=='alterar'){
+	$id_user=$_GET['id'];
+	
+	$sql=mysql_query("select * from users where id_user='$id_user'");
+while($linha=mysql_fetch_array($sql)){
+	$id_user=$linha['id_user'];
+	$nome=$linha['nome'];
+	$data_nascimento=$linha['data_nascimento'];
+	$email=$linha['email'];
+	$sexo=$linha['sexo'];
+	$morada=$linha['morada'];
+	$contacto=$linha['contacto'];
+	$estado_civil=$linha['estado_civil'];
 
-
+}
+?>
+<form action="?pg=home&pg_m=informacoes&funcao=alter&id=<?php echo $id_user; ?>" method="post">
+<div class="title">Alterar Informações</div>
+<table cellpadding="5" cellspacing="5" border="0">
+<tr><td><strong>Nome:</strong></td><td><input type="text" name="nome" value="<?php echo $nome; ?>" size="60" /></td></tr>
+<tr><td><strong>Morada:</strong></td><td><input type="text" name="morada" value="<?php echo $morada; ?>" size="30" /> </td></tr>
+<tr><td><strong>Contacto:</strong></td><td><input type="text" name="contacto" value="<?php echo $contacto; ?>" size="10" /></td></tr>
+<tr><td><strong>Sexo:</strong></td><td><select name="sexo">
+											<option value="<?php echo $sexo; ?>"><?php echo $sexo; ?></option>
+                                            <option value=""></option>
+                                            <option value="M">M</option>
+                                            <option value="F">F</option>
+                                            </select></td></tr>
+<tr><td><strong>Data de Nascimento:</strong></td><td><input type="text" name="data_nascimento" value="<?php echo $data_nascimento; ?>" /></td></tr>
+<tr><td><strong>Estado Civil:</strong></td><td><input type="text" name="estado_civil" value="<?php echo $estado_civil; ?>" /></td></tr>
+<tr><td><input type="submit" value="Alterar" /></td><td><a href="?pg=home&pg_m=informacoes">Voltar</a></tr>
+</table>
+</form>
+<?php } ?>
+<?php if($funcao=='alter'){
+	$id=$_GET['id'];
+	$nome=$_POST['nome'];
+	$morada=$_POST['morada'];
+	$sexo=$_POST['sexo'];
+	$data_nascimento=$_POST['data_nascimento'];
+	$estado=$_POST['estado_civil'];
+	$contacto=$_POST['contacto'];
+	
+	$sql=mysql_query("update users set nome='$nome', data_nascimento='$data_nascimento', sexo='$sexo', morada='$morada', contacto='$contacto', estado_civil='$estado' where id_user='$id'");
+		echo"
+					<meta http-equiv=refresh content='0; url=index.php?pg=home&pg_m=informacoes&funcao=alterar&id=$id'>
+					
+					";
+	
+} ?>
